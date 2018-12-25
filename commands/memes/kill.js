655dd485061dc91fd44cb63d20748a34ac8f452e
runarmod <40912933+runarmod@users.noreply.github.com>
2018-12-26 00:14:43 +0100

    Update kill.js

--- commands/memes/kill.js
+++ commands/memes/kill.js
@@ -14,7 +14,14 @@ class KillHimCommand extends Commando.Command
 
     async run(message, args)
     {
-        message.channel.sendMessage('Ahh, that backfired... I killed ' + message.author + ' instead. Oops :/');
+        if(message.content.includes(message.author))
+        {
+            message.channel.sendMessage('Sorry, but I will not give you permission to kill yourself ' + message.author + '. Sorry :/');
+        }
+        else
+        {
+            message.channel.sendMessage('Ahh, that backfired... I killed ' + message.author + ' instead. Oops :/');
+        }
     }
 }
 
