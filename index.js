031dbd4971c2ee1654572ad4d8e11d5ee78d2433
runarmod <40912933+runarmod@users.noreply.github.com>
2019-01-06 01:18:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ bot.on('message', message => {
 
 bot.on('ready', function(){
     bot.user.setActivity("'help")
-    bot.channels.get("523176389591957524").send("My code just got updated!");
+    bot.channels.find(channel => channel.name === "online").send("My code just got updated!");
     /*
     var millisTillNewYear = new Date(2020, 0, 1, 0, 0).getTime() - Date.now() - 3600000;
     setTimeout(function(){
