309bdd20c656c530b21339dbe7b856f221ab85a7
runarmod <40912933+runarmod@users.noreply.github.com>
2019-01-07 19:07:54 +0100

    Add files via upload

---
+++ commands/mod/botsend.js
@@ -0,0 +1,35 @@
+var store = require('amplify-store');
+const Commando = require('discord.js-commando');
+
+class BotSendCommand extends Commando.Command
+{
+    constructor(client)
+    {
+       super(client,{
+           name: 'botsend',
+           group: 'mod',
+           memberName: 'botsend',
+           description: 'Makes the bot send a message in current channel. (for mods only)'
+       });
+    }
+
+    async run(message, args)
+    {
+        var mods = store( "moderators" );
+        if(mods.includes(message.author.id))
+        {
+            message.channel.send(args);
+            message.delete();
+        }
+        else
+        {
+            message.channel.send(message.author + ", you don't have permission to use this command...")
+            .then(msg => {
+                message.delete(3000);
+                msg.delete(3000);
+            })
+        }
+    }
+}
+
+module.exports = BotSendCommand;
