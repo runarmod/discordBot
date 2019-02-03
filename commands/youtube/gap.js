adcde071d4ee643335bb55fd3175b39abffe75d2
runarmod <40912933+runarmod@users.noreply.github.com>
2019-02-03 13:28:29 +0100

    Update gap.js

--- commands/youtube/gap.js
+++ commands/youtube/gap.js
@@ -18,24 +18,34 @@ class GapYoutubeCommand extends Commando.Command {
         var totalsubscribersT;
         var urlPew;
         var urlT;
+        var dataPewWorked;
+        var dataTWorked;
         var apikey = process.env.youtubeapi;
 
         urlPew = "https://www.googleapis.com/youtube/v3/channels?key=" + apikey + "&id=UC-lHJZR3Gqxm24_Vd_AJ5Yw&part=snippet,contentDetails,statistics";
         urlT = "https://www.googleapis.com/youtube/v3/channels?key=" + apikey + "&id=UCq-Fj5jknLsUf-MWSy4_brA&part=snippet,contentDetails,statistics";
 
         request(urlPew, { json: true }, (err, res, dataPew) => {
+            if (!Array.isArray(dataPew.items) || !dataPew.items.length) {
+                return message.reply("an error has occured. Try again or contact runarmod#4352")
+            }
+            dataPewWorked = true;
             fetchDataPew(dataPew);
         });
+        if (dataPewWorked != true) return;
         request(urlT, { json: true }, (err, res, dataT) => {
+            if (!Array.isArray(dataT.items) || !dataT.items.length) {
+                return message.reply("an error has occured. Try again or contact runarmod#4352")
+            }
+            dataTWorked = true;
             fetchDataT(dataT);
         });
+        if (dataTWorked != true) return;
 
         function fetchDataPew(dataPew) {
-            if (!Array.isArray(dataPew.items) || !dataPew.items.length) {return message.reply("an error has occured. Try again or contact runarmod#4352")}
             totalsubscribersPew = dataPew.items[0].statistics.subscriberCount;
         }
         function fetchDataT(dataT) {
-            if (!Array.isArray(dataT.items) || !dataT.items.length) {return message.reply("an error has occured. Try again or contact runarmod#4352")}
             totalsubscribersT = dataT.items[0].statistics.subscriberCount;
 
         }
